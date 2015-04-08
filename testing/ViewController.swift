27b2aaab24001758b3400c2bf39e96e9e2bfa484
Dharmesh <khenid21@gmail.com>
2015-04-08 10:06:15 +0530

    Play audio with swift
    Play audio with swift

---
+++ testing/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by Anil on 08/04/15.
+//  Copyright (c) 2015 Variya Soft Solutions. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
+    @IBAction func play(sender: AnyObject) {
+        var audioPath = NSString(string: NSBundle.mainBundle().pathForResource("1", ofType: "mp3")!)
+        
+        var error: NSError? = nil
+        
+        //instantiate the player
+        player = AVAudioPlayer(contentsOfURL: NSURL(string: audioPath), error: &error)
+        player.prepareToPlay()
+        player.play()
+    }
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        player.pause()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
